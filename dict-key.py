4a1af059672c4e8cbff06511c955f8d35a6babde
James Shapiro <james.shapiro@gmail.com>
2017-04-05 00:47:00 -0700

    add integer-float == example

--- dict-key.py
+++ dict-key.py
@@ -10,14 +10,20 @@ Output:
 first_cat == second_cat? True!
 first_cat is second_cat? False!
 American Shorthair
+zero point zero
 """
 
-first_cat = "c" + "at"
+dict = {}
+first_cat = "cat"
 second_cat = "ca"
 second_cat = second_cat + "t"
-print("first_cat == second_cat? {}!".format(first_cat == second_cat))
-print("first_cat is second_cat? {}!".format(first_cat is second_cat))
-dict = {}
+print("first_cat == second_cat: {}".format(first_cat == second_cat))
+print("first_cat is second_cat: {}".format(first_cat is second_cat))
 dict[first_cat] = "Siamese"
 dict[second_cat] = "American Shorthair"
 print(dict[first_cat])
+
+dict[0] = 'zero'
+dict[0.0] = 'zero point zero'
+print(dict[0])
+
